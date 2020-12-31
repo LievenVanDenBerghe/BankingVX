713712a50a1f7e6308f989b0204c433bf2ed6bd0
Lieven VanDen Berghe <lieven.vandenberghe.w9944@student.hogent.be>
2020-12-31 13:19:35 +0100

    commit 1

---
+++ BankingVX/Models/Domain/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BankingVX.Models.Domain
+{
+    public class BankAccount
+    {
+
+        #region fields
+
+        #endregion
+
+        #region properties
+        public string AccountNumber { get; }
+
+        public decimal Balance { get; private set; }
+
+        #endregion
+
+        #region constructors
+        public BankAccount(string account)
+        {
+            AccountNumber = account;
+            Balance = Decimal.Zero;
+        }
+        #endregion
+
+        #region methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
--- BankingVX/Program.cs
+++ BankingVX/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using BankingVX.Models.Domain;
+using System;
 
 namespace BankingVX
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myAccount = new BankAccount("123-4567890-12");
+            Console.WriteLine($"AccountNumber: {myAccount.AccountNumber}");
+            Console.WriteLine($"Balance: {myAccount.Balance}");
+            myAccount.Deposit(200M);
+            Console.WriteLine($"Balance after deposit of 200 euros: {myAccount.Balance}");
+            myAccount.Withdraw(100M);
+            Console.WriteLine($"Balance after deposit of 200 euros: {myAccount.Balance}");
+            Console.WriteLine();
         }
     }
 }
